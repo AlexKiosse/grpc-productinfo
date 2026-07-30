37f0c961ff2161a0fda1914154bbe903135442d2
AlexKiosse <perryplatypus2007@gmail.com>
2026-07-30 23:03:29 +0300

    add mTLS support and certificate generation

--- client/product/product_client.go
+++ client/product/product_client.go
@@ -2,27 +2,67 @@ package main
 
 import (
 	"context"
+	"crypto/tls"
+	"crypto/x509"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"time"
 
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
-	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/credentials"
 	"google.golang.org/grpc/status"
 
 	"productinfo/client/common"
 	pb "productinfo/client/ecommerce"
 )
 
+var (
+	hostname = "localhost"
+	crtFile  = "../../certs/client/crt"
+	keyFile  = "../../certs/client.key"
+	caFile   = "../../certs/ca.crt"
+)
+
 func main() {
-	conn, err := grpc.Dial("localhost:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
+	// Загружаем ключ и сертификат клиента
+	certificate, err := tls.LoadX509KeyPair(crtFile, keyFile)
+	if err != nil {
+		log.Fatalf("Failed to load the server certificate and key: %v", err)
+	}
+
+	// Создаём пул доверенных сертификатов для проверки сервера
+	certPool := x509.NewCertPool()
+	ca, err := ioutil.ReadFile(caFile)
+	if err != nil {
+		log.Fatalf("Failed to read the CA certificate: %v", err)
+	}
+
+	// Добавляем CA в пул
+	if ok := certPool.AppendCertsFromPEM(ca); !ok {
+		log.Fatalf("Failed to add the CA to the pool")
+	}
+
+	// Настраиваем TLS для клиента
+	tlsConfig := &tls.Config{
+		Certificates: []tls.Certificate{certificate},
+		ServerName:   hostname,
+		RootCAs:      certPool,
+		MinVersion:   tls.VersionTLS12,
+	}
+
+	// Создаём учётные данные TLS
+	creds := credentials.NewTLS(tlsConfig)
+
+	conn, err := grpc.Dial("localhost:50051", grpc.WithTransportCredentials(creds))
 	if err != nil {
 		log.Fatalf("Product client: did not connect: %v", err)
 	}
 	defer conn.Close()
 
 	client := pb.NewProductInfoClient(conn)
+
 	// Создаём контекст с метаданными
 	ctx := common.WithAuthMetadata(context.Background())
 
--- service/main.go
+++ service/main.go
@@ -3,34 +3,74 @@ package main
 import (
 	pb "productinfo/service/ecommerce"
 
+	"crypto/tls"
+	"crypto/x509"
+	"io/ioutil"
 	"log"
 	"net"
 
 	grpc_middleware "github.com/grpc-ecosystem/go-grpc-middleware"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
 )
 
 const (
 	port = ":50051"
 )
 
+var (
+	crtFile = "../certs/server.crt"
+	keyFile = "../certs/server.key"
+	caFile  = "../certs/ca.crt"
+)
+
 func main() {
-	lis, err := net.Listen("tcp", port)
+	// Загружаем ключ и сертификат сервера
+	certificate, err := tls.LoadX509KeyPair(crtFile, keyFile)
 	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
+		log.Fatalf("Failed to load the server certificate and key: %v", err)
 	}
 
-	// Подключаем цепочку перехватчиков
+	// Создаём пул доверенных сертификатов для проверки клиентов
+	certPool := x509.NewCertPool()
+	ca, err := ioutil.ReadFile(caFile)
+	if err != nil {
+		log.Fatalf("Failed to read the CA certificate: %v", err)
+	}
+
+	// Добавляем CA в пул
+	if ok := certPool.AppendCertsFromPEM(ca); !ok {
+		log.Fatalf("Failed to add the CA to the pool")
+	}
+
+	// Настройка mTLS
+	tlsConfig := &tls.Config{
+		Certificates: []tls.Certificate{certificate},
+		ClientAuth:   tls.RequireAndVerifyClientCert,
+		ClientCAs:    certPool,
+		MinVersion:   tls.VersionTLS12,
+	}
+
+	// Создаём учётные данные TLS
+	creds := credentials.NewTLS(tlsConfig)
+
+	// Создаём grpc-сервер с mTLS и подключаем цепочку перехватчиков
 	s := grpc.NewServer(
 		grpc.UnaryInterceptor(grpc_middleware.ChainUnaryServer(
 			recoveryInterceptor, // зашита от падений
 			authInterceptor,     // проверка авторизации
 			loggingInterceptor,  // логирование
 		)),
+		grpc.Creds(creds),
 	)
 
 	pb.RegisterProductInfoServer(s, &server{})
 	pb.RegisterOrderManagementServer(s, &orderServer{})
+
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
 
 	log.Printf("Starting gRPC listener on port " + port)
 	if err := s.Serve(lis); err != nil {
